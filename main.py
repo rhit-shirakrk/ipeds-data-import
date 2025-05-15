07ef5db2aa3ab579a64066f511c1cdf26ee038dc
Ryan Shiraki <shirakrk@rose-hulman.edu>
2025-05-15 03:25:12 -0400

    implemented usage of table creator in main

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ import argparse
 import pathlib
 import sys
 
+import win32com.client
+
 import access_db
 import db_config
+import table_creator
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -19,9 +22,23 @@ if __name__ == "__main__":
         print("Path to Microsoft Access DB file required")
         sys.exit(1)
 
-    db_conn = db_config.DBManager(args.config_file)
+    # used for fetching primary keys in tables
+    db_engine = win32com.client.Dispatch("DAO.DBEngine.120")
+    win32_db = db_engine.OpenDatabase(MDB)
 
-    access_db_file_path = pathlib.Path(args.access_db_file)
+    # used to connect to MySQL DB
+    mysql_conn = db_config.DBManager(args.config_file).get_db_connection()
+    mysql_cursor = mysql_conn.cursor()
 
-    access_db_conn_manager = access_db.AccessDBConnManager(access_db_file_path)
-    access_db_conn = access_db_conn_manager.get_connection()
+    # used for general-purpose tasks in Microsoft Access DB
+    access_db_conn = access_db.AccessDBConnManager(
+        pathlib.Path(args.access_db_file)
+    ).get_connection()
+    access_db_cursor = access_db_conn.cursor()
+
+    # create MySQL tables
+    tc = table_creator.TableCreator(access_db_cursor, mysql_cursor, win32_db)
+    for table_info in access_db_cursor.tables(tableType="TABLE"):
+        tc.create_table(table_info.table_name)
+
+    # populate MySQL tables with data
--- table_creator.py
+++ table_creator.py
@@ -2,33 +2,6 @@
 Creates MySQL tables from Microsoft Access DB tables
 """
 
-import pandas as pd
-import win32com.client
-
-
-class MySQLTable:
-    """Representation of a MySQL table resulting from a Microsoft Access DB table
-
-    :param table_name: The name of the table in MySQL
-    :type table_name: str
-    :param column_name_to_datatype: A mapping between each column and its expected
-    datatype
-    :type column_name_to_datatype: dict[str, Any]
-    """
-
-    def __init__(
-        self, table_name: str, column_name_to_datatype: dict[str, Any]
-    ) -> None:
-        self.table_name = table_name
-        self.column_name_to_datatype = column_name_to_datatype
-
-    def generate_empty_table(self) -> pd.DataFrame:
-        """Generate table from specified table name and column datatype mappings
-
-        :return: A Python representation of the table
-        :rtype: pd.DataFrame
-        """
-
 
 class TableCreator:
     """
@@ -47,45 +20,15 @@ class TableCreator:
         :param table_name: The name of the table to create in Python
         :type table_name: str
         """
-        self.mysql_cursor.execute(self._generate_create_table_query(table_name))
-
-    def _generate_create_table_query(self, table_name: str) -> str:
-        """Generate a CREATE TABLE MySQL query based on the column names,
-        types, and schematics of the table
-
-        :param table_name: The name of the table to create in Python
-        :type table_name: str
-        :return: A query string to create the schematics of an Access DB table
-        in MySQL
-        :rtype: str
-        """
-        table_parameters = self._generate_table_parameters(table_name)
-        table_parameters.append(self._generate_primary_keys_as_table_parameter())
-
-        return f"CREATE TABLE {table_name}({table_parameters})"
-
-    def _generate_table_parameters(self, table_name: str) -> list[str]:
-        """Generate a list of table parameters mapping columns to their data type
-
-        :param table_name: The name of the table to create in Python
-        :type table_name: str
-        :return: A list of columns and their data types
-        :rtype: list[str]
-        """
-        return [
+        column_parameters = [
             f"{row.column_name} {row.type_name}"
-            for row in access_db_cursor.columns(table=table_name)
+            for row in self.access_db_cursor.columns(table=table_name)
         ]
+        column_parameters.append(
+            f"PRIMARY KEY ({', '.join(self._get_primary_keys(table_name))})"
+        )
 
-    def _generate_primary_keys_as_table_parameter(self, table_name: str) -> str:
-        """Generate a string that sets primary keys on a MySQL table
-
-        :param table_name: The name of the table to create in Python
-        :type table_name: str
-        :return: Primary key table constraints
-        :rtype: str
-        """
-        return f"PRIMARY KEY ({', '.join(self._get_primary_keys(table_name))})"
+        self.mysql_cursor.execute(f"CREATE TABLE {table_name}({column_parameters})")
 
     def _get_primary_keys(self, table_name: str) -> list[str]:
         """Get primary keys associated with a table
