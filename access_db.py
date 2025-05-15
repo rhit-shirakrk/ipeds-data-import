d902044b3359603392ebf424f74de7c9343b5e58
Ryan Shiraki <shirakrk@rose-hulman.edu>
2025-05-15 00:34:44 -0400

    created module to handle accessdb connections using pyodbc

---
+++ access_db.py
@@ -0,0 +1,22 @@
+"""
+Manage Microsoft Access DB connection
+"""
+
+import pathlib
+
+import pyodbc
+
+
+class AccessDBConnManager:
+    DRIVER = "Microsoft Access Driver (*.mdb, *.accdb)"
+
+    def __init__(self, file_path: pathlib.Path) -> None:
+        self.file_path = file_path
+
+    def get_connection(self):
+        """
+        Return connection to Microsoft Access DB file
+        """
+        return pyodbc.connect(
+            f"DRIVER={AccessDBConnManager.DRIVER}, DBQ={self.file_path}"
+        )
